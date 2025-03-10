b46c8cde6aac320ab185dcc4e2e63e0e5f263e6a
김의중 <ejk5148@gmail.com>
2025-03-10 10:27:04 +0900

    [init] 스프링부트 초기설정

---
+++ backend/src/main/java/io/ssafy/p/s12b201/techmate/TechmateApplication.java
@@ -0,0 +1,13 @@
+package io.ssafy.p.s12b201.techmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmateApplication.class, args);
+	}
+
+}
