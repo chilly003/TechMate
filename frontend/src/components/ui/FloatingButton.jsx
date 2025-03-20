7edd34bbe8cfbfda95371ab36a66b7b316c0d1f7
이상호 <tkdgh6427@naver.com>
2025-03-20 17:10:46 +0900

    feat : floatingButton

--- frontend/src/components/ui/FloatingButton.jsx
+++ frontend/src/components/ui/FloatingButton.jsx
@@ -1,9 +1,18 @@
-const FloatingButton = ({ text, color }) => {
+const FloatingButton = ({ text, color, onClick }) => {
+    const splitText = text.split(" ");
+
     return (
         <button
-            className={`bg-${color}`}
+            onClick={onClick}
+            className={`flex fixed bottom-4 right-4 items-center justify-center w-14 h-14 rounded-full shadow-lg 
+        bg-gradient-to-b ${color} text-white text-xs font-bold
+        transition-all duration-200 ease-in-out
+        hover:scale-110 hover:shadow-xl
+        active:scale-105 active:brightness-90`}
         >
-            {text}
+            {splitText.map((line, index) => (
+                <span key={index}>{line}</span>
+            ))}
         </button>
     );
 };
