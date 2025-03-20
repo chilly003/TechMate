37696568605c514d7e06acf744c60ff4f6acd3af
김의중 <ejk5148@gmail.com>
2025-03-20 16:32:23 +0900

    [feat] 사용자 선호기사 엔터티 생성 #S12P21B201-72

--- backend/src/main/java/io/ssafy/p/s12b201/techmate/domain/user/domain/User.java
+++ backend/src/main/java/io/ssafy/p/s12b201/techmate/domain/user/domain/User.java
@@ -3,6 +3,7 @@ package io.ssafy.p.s12b201.techmate.domain.user.domain;
 import io.ssafy.p.s12b201.techmate.domain.articlelike.domain.ArticleLike;
 import io.ssafy.p.s12b201.techmate.domain.quizresult.domain.QuizResult;
 import io.ssafy.p.s12b201.techmate.domain.articleread.domain.ArticleRead;
+import io.ssafy.p.s12b201.techmate.domain.userpreference.domain.UserPreference;
 import io.ssafy.p.s12b201.techmate.global.database.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -31,6 +32,9 @@ public class User extends BaseEntity {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private final List<QuizResult> quizResultList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private final List<UserPreference> userPreferenceList = new ArrayList<>();
 
     @ElementCollection
     @CollectionTable(name = "user_preferences", joinColumns =
