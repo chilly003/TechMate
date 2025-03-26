54f4937c86380b234007dd9c4c4a74196de02091
CHJ <shine8648@naver.com>
2025-03-26 14:02:27 +0900

    [fix] 사용자 프로필 api url 수정

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -12,7 +12,7 @@ api.interceptors.request.use(
         // }
         // return config;
 
-        config.headers.Authorization = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ0ZWNobWF0ZSIsImlhdCI6MTc0MjgwMTY5OCwic3ViIjoiMSIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJyb2xlIjoiVVNFUiIsImV4cCI6MTc0Mjk4MTY5OH0.zQE9t8dk8MLkCyhYyC6tNE5mxyfA9FBIcdibCIZpZOM';
+        config.headers.Authorization = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ0ZWNobWF0ZSIsImlhdCI6MTc0MjgwMDE0Miwic3ViIjoiOSIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJyb2xlIjoiVVNFUiIsImV4cCI6MTc0Mjk4MDE0Mn0.DmXrBJJzDgXD9JHwxIWzsXTjWz7Cjb0CHfOt9tv_lp8';
 
         return config;
     },
