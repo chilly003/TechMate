49cfa0f1c34fc453d01a8a1f68e0ff42691cda67
chilly003 <chfhchf03@naver.com>
2025-04-07 13:17:11 +0900

    분기처리 완료

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
-import './App.css'
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/common/Header';
 import Intro from './pages/IntroPage';
 import ArticlePage from './pages/ArticlePage';
-// import ArticlePage from './pages/articlepage';
 import HomePage from './pages/HomePage';
 import Mypage from './pages/MyPage';
 import UserProfilePage from './pages/UserProfilePage';
@@ -11,24 +10,28 @@ import OpenPage from './pages/OpenPage';
 import KakaoCallback from './pages/KakaoCallback';
 import GoogleCallback from './pages/GoogleCallback';
 
-
 function App() {
+  const accessToken = localStorage.getItem('accessToken');
+
   return (
-    // access 토큰 있을 때만 헤더 보여주기
-    // access 토큰 없다면 open, / 화면만 볼 수 있음.
     <Router>
       <div>
-        <Header />
-        <div> {/* Removed pt-16 padding */}
+        {accessToken && <Header/>}
+        <div>
           <Routes>
             <Route path="/" element={<OpenPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/userprofile" element={<UserProfilePage />} />
-            <Route path="/article/:id" element={<ArticlePage />} />
-            <Route path="/mypage" element={<Mypage />} />
             <Route path="/open" element={<Intro />} />
-            <Route path="/auth" element={<KakaoCallback />} />
-            <Route path="/auth/google" element={<GoogleCallback />} />
+
+            {accessToken && (
+              <>
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/userprofile" element={<UserProfilePage />} />
+                <Route path="/article/:id" element={<ArticlePage />} />
+                <Route path="/mypage" element={<Mypage />} />
+                <Route path="/auth" element={<KakaoCallback />} />
+                <Route path="/auth/google" element={<GoogleCallback />} />
+              </>
+            )}
           </Routes>
         </div>
       </div>
