fd1a407584dc90b1ca23493d4bc5a611516a3246
chilly003 <chfhchf03@naver.com>
2025-04-07 14:57:13 +0900

    화면 분기 처리 라우터 수정 완료

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,56 +1,64 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import Header from './components/common/Header';
-import Intro from './pages/IntroPage';
-import ArticlePage from './pages/ArticlePage';
-import HomePage from './pages/HomePage';
-import Mypage from './pages/MyPage';
-import UserProfilePage from './pages/UserProfilePage';
-import OpenPage from './pages/OpenPage';
-import KakaoCallback from './pages/KakaoCallback';
-import GoogleCallback from './pages/GoogleCallback';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/common/Header";
+import Intro from "./pages/IntroPage";
+import ArticlePage from "./pages/ArticlePage";
+import HomePage from "./pages/HomePage";
+import Mypage from "./pages/MyPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import OpenPage from "./pages/OpenPage";
+import KakaoCallback from "./pages/KakaoCallback";
+import GoogleCallback from "./pages/GoogleCallback";
+import ProtectedRoute from "./pages/ProtectedRoute"; // Route Guard 컴포넌트
 
 function App() {
-  const [accessToken, setAccessToken] = useState(localStorage.getItem('accessToken'));
-
-  useEffect(() => {
-    // localStorage 변경 감지 및 상태 업데이트
-    const handleStorageChange = () => {
-      setAccessToken(localStorage.getItem('accessToken'));
-    };
-
-    // storage 이벤트 리스너 등록
-    window.addEventListener('storage', handleStorageChange);
-
-    // 컴포넌트 언마운트 시 리스너 제거
-    return () => {
-      window.removeEventListener('storage', handleStorageChange);
-    };
-  }, []);
 
   return (
     <Router>
       <div>
-        {accessToken && <Header />} 
-        <div>
           <Routes>
+            {/* 엑세스 토큰 여부와 상관없이 접근 가능한 경로 */}
             <Route path="/" element={<OpenPage />} />
             <Route path="/open" element={<Intro />} />
-
-            {accessToken && (
-              <>
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/article/:id" element={<ArticlePage />} />
-                <Route path="/mypage" element={<Mypage />} />
-              </>
-            )}
-
             <Route path="/userprofile" element={<UserProfilePage />} />
             <Route path="/auth" element={<KakaoCallback />} />
             <Route path="/auth/google" element={<GoogleCallback />} />
+
+            {/* 엑세스 토큰이 필요할 때만 접근 가능한 경로 */}
+            <Route
+              path="/home"
+              element={
+                <ProtectedRoute>
+                  <>
+                  <Header />
+                  <HomePage />
+                  </>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/article/:id"
+              element={
+                <ProtectedRoute>
+                  <>
+                  <Header />
+                  <ArticlePage />
+                  </>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/mypage"
+              element={
+                <ProtectedRoute>
+                  <>
+                  <Header />
+                  <Mypage />
+                  </>
+                </ProtectedRoute>
+              }
+            />
           </Routes>
-        </div>
       </div>
     </Router>
   );
